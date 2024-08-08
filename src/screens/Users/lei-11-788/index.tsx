4439a687bdcb6c428bab5e3db7a3ec9cb46c75a1
gabrielmerola <gabrielmerola12@gmail.com>
2024-08-08 13:44:55 -0300

    tela da lei

--- src/screens/Users/lei-11-788/index.tsx
+++ src/screens/Users/lei-11-788/index.tsx
@@ -6,7 +6,125 @@ export function Lei() {
         <>
         <Header page="lei-11-788"/>
         <main>
-            <h1>LEI 11.788</h1>
+            <section className="bg-prime-blue p-8 text-white flex justify-center">
+                <h1 className="w-1/2 font-bold text-3xl">Lei 11.788</h1>
+            </section>
+            <section className="flex justify-center my-4">
+                <div className="w-1/2">
+                    <p><strong>Lei N. 11.788, de 25 de Setembro de 2008.</strong></p>
+                    <p>&nbsp;</p>
+                    <p><strong>O PRESIDENTE DA REPÚBLICA</strong> Faço saber que o Congresso Nacional decreta e eu sanciono a seguinte Lei:</p>
+                    <p>&nbsp;</p>
+                    <p>CAPÍTULO I</p>
+                    <p>DA DEFINIÇÃO, CLASSIFICAÇÃO E RELAÇÕES DE ESTÁGIO</p>
+                    <p><strong>Art. 1º</strong> Estágio é ato educativo escolar supervisionado, desenvolvido no ambiente de trabalho, que visa à preparação para o trabalho produtivo de educandos que estejam freqüentando o ensino regular em instituições de educação superior, de educação profissional, de ensino médio, da educação especial e dos anos finais do ensino fundamental, na modalidade profissional da educação de jovens e adultos.</p>
+                    <p><strong>§ 1º</strong> O estágio faz parte do projeto pedagógico do curso, além de integrar o itinerário formativo do educando.</p>
+                    <p><strong>§ 2º</strong> O estágio visa ao aprendizado de competências próprias da atividade profissional e à contextualização curricular, objetivando o desenvolvimento do educando para a vida cidadã e para o trabalho.</p>
+                    <p><strong>Art. 2º</strong> O estágio poderá ser obrigatório ou não-obrigatório, conforme determinação das diretrizes curriculares da etapa, modalidade e área de ensino e do projeto pedagógico do curso.</p>
+                    <p><strong>§ 1º</strong> Estágio obrigatório é aquele definido como tal no projeto do curso, cuja carga horária é requisito para aprovação e obtenção de diploma.</p>
+                    <p><strong>§ 2º</strong> Estágio não-obrigatório é aquele desenvolvido como atividade opcional, acrescida à carga horária regular e obrigatória.</p>
+                    <p><strong>§ 3º</strong> As atividades de extensão, de monitorias e de iniciação científica na educação superior, desenvolvidas pelo estudante, somente poderão ser equiparadas ao estágio em caso de previsão no projeto pedagógico do curso.</p>
+                    <p><strong>Art. 3º</strong> O estágio, tanto na hipótese do § 1º do art. 2º desta Lei quanto na prevista no § 2º do mesmo dispositivo, não cria vínculo empregatício de qualquer natureza, observados os seguintes requisitos:</p>
+                    <p><strong>I –</strong> matrícula e freqüência regular do educando em curso de educação superior, de educação profissional, de ensino médio, da educação especial e nos anos finais do ensino fundamental, na modalidade profissional da educação de jovens e adultos e atestados pela instituição de ensino;</p>
+                    <p><strong>II –</strong> celebração de termo de compromisso entre o educando, a parte concedente do estágio e a instituição de ensino;</p>
+                    <p><strong>III –</strong> compatibilidade entre as atividades desenvolvidas no estágio e aquelas previstas no termo de compromisso.</p>
+                    <p><strong>§ 1º</strong> O estágio, como ato educativo escolar supervisionado, deverá ter acompanhamento efetivo pelo professor orientador da instituição de ensino e por supervisor da parte concedente, comprovado por vistos nos relatórios referidos no inciso IV do caput do art. 7º desta Lei e por menção de aprovação final.</p>
+                    <p><strong>§ 2º</strong> O descumprimento de qualquer dos incisos deste artigo ou de qualquer obrigação contida no termo de compromisso caracteriza vínculo de emprego do educando com a parte concedente do estágio para todos os fins da legislação trabalhista e previdenciária.</p>
+                    <p><strong>Art. 4º</strong> A realização de estágios, nos termos desta Lei, aplica-se aos estudantes estrangeiros regularmente matriculados em cursos superiores no País, autorizados ou reconhecidos, observado o prazo do visto temporário de estudante, na forma da legislação aplicável.</p>
+                    <p><strong>Art. 5º</strong> As instituições de ensino e as partes cedentes de estágio podem, a seu critério, recorrer a serviços de agentes de integração públicos e privados, mediante condições acordadas em instrumento jurídico apropriado, devendo ser observada, no caso de contratação com recursos públicos, a legislação que estabelece as normas gerais de licitação.</p>
+                    <p><strong>§ 1º</strong> Cabe aos agentes de integração, como auxiliares no processo de aperfeiçoamento do instituto do estágio:</p>
+                    <p><strong>I –</strong> identificar oportunidades de estágio;</p>
+                    <p><strong>II –</strong> ajustar suas condições de realização;</p>
+                    <p><strong>III –</strong> fazer o acompanhamento administrativo;</p>
+                    <p><strong>IV –</strong> encaminhar negociação de seguros contra acidentes pessoais;</p>
+                    <p><strong>V –</strong> cadastrar os estudantes.</p>
+                    <p><strong>§ 2º</strong> É vedada a cobrança de qualquer valor dos estudantes, a título de remuneração pelos serviços referidos nos incisos deste artigo.</p>
+                    <p><strong>§ 3º</strong> Os agentes de integração serão responsabilizados civilmente se indicarem estagiários para a realização de atividades não compatíveis com a programação curricular estabelecida para cada curso, assim como estagiários matriculados em cursos ou instituições para as quais não há previsão de estágio curricular.</p>
+                    <p><strong>Art. 6º</strong> O local de estágio pode ser selecionado a partir de cadastro de partes cedentes, organizado pelas instituições de ensino ou pelos agentes de integração.</p>
+                    <p>&nbsp;</p>
+                    <p>CAPÍTULO II</p>
+                    <p>DA INSTITUIÇÃO DE ENSINO</p>
+                    <p><strong>Art. 7º</strong> São obrigações das instituições de ensino, em relação aos estágios de seus educandos:</p>
+                    <p><strong>I –</strong> celebrar termo de compromisso com o educando ou com seu representante ou assistente legal, quando ele for absoluta ou relativamente incapaz, e com a parte concedente, indicando as condições de adequação do estágio à proposta pedagógica do curso, à etapa e modalidade da formação escolar do estudante e ao horário e calendário escolar;</p>
+                    <p><strong>II –</strong> avaliar as instalações da parte concedente do estágio e sua adequação à formação cultural e profissional do educando;</p>
+                    <p><strong>III –</strong> indicar professor orientador, da área a ser desenvolvida no estágio, como responsável pelo acompanhamento e avaliação das atividades do estagiário;</p>
+                    <p><strong>IV –</strong> exigir do educando a apresentação periódica, em prazo não superior a 6 (seis) meses, de relatório das atividades;</p>
+                    <p><strong>V –</strong> zelar pelo cumprimento do termo de compromisso, reorientando o estagiário para outro local em caso de descumprimento de suas normas;</p>
+                    <p><strong>VI –</strong> elaborar normas complementares e instrumentos de avaliação dos estágios de seus educandos;</p>
+                    <p><strong>VII –</strong> comunicar à parte concedente do estágio, no início do período letivo, as datas de realização de avaliações escolares ou acadêmicas.</p>
+                    <p><strong>Parágrafo único.</strong> O plano de atividades do estagiário, elaborado em acordo das 3 (três) partes a que se refere o inciso II do caput do art. 3º desta Lei, será incorporado ao termo de compromisso por meio de aditivos à medida que for avaliado, progressivamente, o desempenho do estudante.</p>
+                    <p><strong>Art. 8º</strong> É facultado às instituições de ensino celebrar com entes públicos e privados convênio de concessão de estágio, nos quais se explicitem o processo educativo compreendido nas atividades programadas para seus educandos e as condições de que tratam os arts. 6º a 14 desta Lei.</p>
+                    <p><strong>Parágrafo único.</strong> A celebração de convênio de concessão de estágio entre a instituição de ensino e a parte concedente não dispensa a celebração do termo de compromisso de que trata o inciso II do caput do art. 3º desta Lei.</p>
+                    <p>&nbsp;</p>
+                    <p>CAPÍTULO III</p>
+                    <p>DA PARTE CONCEDENTE</p>
+                    <p><strong>Art. 9º</strong> As pessoas jurídicas de direito privado e os órgãos da administração pública direta, autárquica e fundacional de qualquer dos Poderes da União, dos Estados, do Distrito Federal e dos Municípios, bem como profissionais liberais de nível superior devidamente registrados em seus respectivos conselhos de fiscalização profissional, podem oferecer estágio, observadas as seguintes obrigações:</p>
+                    <p><strong>I –</strong> celebrar termo de compromisso com a instituição de ensino e o educando, zelando por seu cumprimento;</p>
+                    <p><strong>II –</strong> ofertar instalações que tenham condições de proporcionar ao educando atividades de aprendizagem social, profissional e cultural;</p>
+                    <p><strong>III –</strong> indicar funcionário de seu quadro de pessoal, com formação ou experiência profissional na área de conhecimento desenvolvida no curso do estagiário, para orientar e supervisionar até 10 (dez) estagiários simultaneamente;</p>
+                    <p><strong>IV –</strong> contratar em favor do estagiário seguro contra acidentes pessoais, cuja apólice seja compatível com valores de mercado, conforme fique estabelecido no termo de compromisso;</p>
+                    <p><strong>V –</strong> por ocasião do desligamento do estagiário, entregar termo de realização do estágio com indicação resumida das atividades desenvolvidas, dos períodos e da avaliação de desempenho;</p>
+                    <p><strong>VI –</strong> manter à disposição da fiscalização documentos que comprovem a relação de estágio;</p>
+                    <p><strong>VII –</strong> enviar à instituição de ensino, com periodicidade mínima de 6 (seis) meses, relatório de atividades, com vista obrigatória ao estagiário.</p>
+                    <p><strong>Parágrafo único. No caso de estágio obrigatório, a responsabilidade pela contratação do seguro de que trata o inciso IV do caput deste artigo poderá, alternativamente, ser assumida pela instituição de ensino.</strong></p>
+                    <p>&nbsp;</p>
+                    <p>CAPÍTULO IV</p>
+                    <p>DO ESTAGIÁRIO</p>
+                    <p><strong>Art. 10</strong> A jornada de atividade em estágio será definida de comum acordo entre a instituição de ensino, a parte concedente e o aluno estagiário ou seu representante legal, devendo constar do termo de compromisso ser compatível com as atividades escolares e não ultrapassar:</p>
+                    <p><strong>I –</strong> 4 (quatro) horas diárias e 20 (vinte) horas semanais, no caso de estudantes de educação especial e dos anos finais do ensino fundamental, na modalidade profissional de educação de jovens e adultos;</p>
+                    <p><strong>II –</strong> 6 (seis) horas diárias e 30 (trinta) horas semanais, no caso de estudantes do ensino superior, da educação profissional de nível médio e do ensino médio regular.</p>
+                    <p><strong>§ 1º</strong> O estágio relativo a cursos que alternam teoria e prática, nos períodos em que não estão programadas aulas presenciais, poderá ter jornada de até 40 (quarenta) horas semanais, desde que isso esteja previsto no projeto pedagógico do curso e da instituição de ensino.</p>
+                    <p><strong>§ 2º</strong> Se a instituição de ensino adotar verificações de aprendizagem periódicas ou finais, nos períodos de avaliação, a carga horária do estágio será reduzida pelo menos à metade, segundo estipulado no termo de compromisso, para garantir o bom desempenho do estudante.</p>
+                    <p><strong>Art. 11</strong> A duração do estágio, na mesma parte concedente, não poderá exceder 2 (dois) anos, exceto quando se tratar de estagiário portador de deficiência.</p>
+                    <p><strong>Art. 12</strong> O estagiário poderá receber bolsa ou outra forma de contraprestação que venha a ser acordada, sendo compulsória a sua concessão, bem como a do auxílio-transporte, na hipótese de estágio não obrigatório.</p>
+                    <p><strong>§ 1º</strong> A eventual concessão de benefícios relacionados a transporte, alimentação e saúde, entre outros, não caracteriza vínculo empregatício.</p>
+                    <p><strong>§ 2º</strong> Poderá o educando inscrever-se e contribuir como segurado facultativo do Regime Geral de Previdência Social.</p>
+                    <p><strong>Art. 13</strong> É assegurado ao estagiário, sempre que o estágio tenha duração igual ou superior a 1 (um) ano, período de recesso de 30 (trinta) dias, a ser gozado preferencialmente durante suas férias escolares.</p>
+                    <p><strong>§ 1º</strong> O recesso de que trata este artigo deverá ser remunerado quando o estagiário receber bolsa ou outra forma de contraprestação.</p>
+                    <p><strong>§ 2º</strong> Os dias de recesso previstos neste artigo serão concedidos de maneira proporcional, nos casos de o estágio ter duração inferior a 1 (um) ano.</p>
+                    <p><strong>Art. 14</strong> Aplica-se ao estagiário a legislação relacionada à saúde e segurança no trabalho, sendo sua implementação de responsabilidade da parte concedente do estágio.</p>
+                    <p>&nbsp;</p>
+                    <p>CAPÍTULO V</p>
+                    <p>DA FISCALIZAÇÃO</p>
+                    <p><strong>Art. 15</strong> A manutenção de estagiários em desconformidade com esta Lei caracteriza vínculo de emprego do educando com a parte concedente do estágio para todos os fins da legislação trabalhista e previdenciária.</p>
+                    <p><strong>§ 1º</strong> A instituição privada ou pública que reincidir na irregularidade de que trata este artigo ficará impedida de receber estagiários por 2 (dois) anos, contados da data da decisão definitiva do processo administrativo correspondente.</p>
+                    <p><strong>§ 2º</strong> A penalidade de que trata o § 1º deste artigo limita-se à filial ou agência em que for cometida a irregularidade.</p>
+                    <p>&nbsp;</p>
+                    <p>CAPÍTULO VI</p>
+                    <p>DAS DISPOSIÇÕES GERAIS</p>
+                    <p><strong>Art. 16</strong> O termo de compromisso deverá ser firmado pelo estagiário ou com seu representante ou assistente legal e pelos representantes legais da parte concedente e da instituição de ensino, vedada a atuação dos agentes de integração a que se refere o art. 5º desta Lei como representante de qualquer das partes.</p>
+                    <p><strong>Art. 17</strong> O número máximo de estagiários em relação ao quadro de pessoal das entidades concedentes de estágio deverá atender às seguintes proporções:</p>
+                    <p><strong>I –</strong> de 1 (um) a 5 (cinco) empregados: 1 (um) estagiário;</p>
+                    <p><strong>II –</strong> de 6 (seis) a 10 (dez) empregados: até 2 (dois) estagiários;</p>
+                    <p><strong>III –</strong> de 11 (onze) a 25 (vinte e cinco) empregados: até 5 (cinco) estagiários;</p>
+                    <p><strong>IV –</strong> acima de 25 (vinte e cinco) empregados: até 20% (vinte por cento) de estagiários.</p>
+                    <p><strong>§ 1º</strong> Para efeito desta Lei, considera-se quadro de pessoal o conjunto de trabalhadores empregados existentes no estabelecimento do estágio.</p>
+                    <p><strong>§ 2º</strong> Na hipótese de a parte concedente contar com várias filiais ou estabelecimentos, os quantitativos previstos nos incisos deste artigo serão aplicados a cada um deles.</p>
+                    <p><strong>§ 3º</strong> Quando o cálculo do percentual disposto no inciso IV do caput deste artigo resultar em fração, poderá ser arredondado para o número inteiro imediatamente superior.</p>
+                    <p><strong>§ 4º</strong> Não se aplica o disposto no caput deste artigo aos estágios de nível superior e de nível médio profissional.</p>
+                    <p><strong>§ 5º</strong> Fica assegurado às pessoas portadoras de deficiência o percentual de 10% (dez por cento) das vagas oferecidas pela parte concedente do estágio.</p>
+                    <p><strong>Art. 18</strong> A prorrogação dos estágios contratados antes do início da vigência desta Lei apenas poderá ocorrer se ajustada às suas disposições.</p>
+                    <p><strong>Art. 19</strong> O art. 428 da Consolidação das Leis do Trabalho – CLT, aprovada pelo Decreto-Lei no 5.452, de 1º de maio de 1943, passa a vigorar com as seguintes alterações:</p>
+                    <blockquote><p>“Art. 428. …………………………………………………………….</p>
+                    <p>§ 1º A validade do contrato de aprendizagem pressupõe anotação na Carteira de Trabalho e Previdência Social, matrícula e freqüência do aprendiz na escola, caso não haja concluído o ensino médio, e inscrição em programa de aprendizagem desenvolvido sob orientação de entidade qualificada em formação técnico-profissional metódica.</p>
+                    <p>…………………………………………………………….</p>
+                    <p>§ 3º O contrato de aprendizagem não poderá ser estipulado por mais de 2 (dois) anos, exceto quando se tratar de aprendiz portador de deficiência.</p>
+                    <p>…………………………………………………………….</p>
+                    <p>§ 7º Nas localidades onde não houver oferta de ensino médio para o cumprimento do disposto no § 1º deste artigo, a contratação do aprendiz poderá ocorrer sem a freqüência à escola, desde que ele já tenha concluído o ensino fundamental.” (NR)</p></blockquote>
+                    <p><strong>Art. 20</strong> O art. 82 da Lei no 9.394, de 20 de dezembro de 1996, passa a vigorar com a seguinte redação:</p>
+                    <blockquote><p>“Art. 82. Os sistemas de ensino estabelecerão as normas de realização de estágio em sua jurisdição, observada a lei federal sobre a matéria.</p>
+                    <p>Parágrafo único. (Revogado).” (NR)</p></blockquote>
+                    <p><strong>Art. 21</strong> Esta Lei entra em vigor na data de sua publicação.</p>
+                    <p><strong>Art. 22</strong> Revogam-se as Leis nos 6.494, de 7 de dezembro de 1977, e 8.859, de 23 de março de 1994, o parágrafo único do art. 82 da Lei no 9.394, de 20 de dezembro de 1996, e o art. 6º da Medida Provisória nº 2.164-41, de 24 de agosto de 2001.</p>
+                    <p>&nbsp;</p>
+                    <blockquote><p>Brasília, 25 de setembro de 2008; 187º da Independência e 120º da República.</p></blockquote>
+                    <p><strong>LUIZ INÁCIO LULA DA SILVA<br/>
+                    <i>Fernando Haddad</i><br/>
+                    <i>André Peixoto Figueiredo Lima</i></strong></p>
+                    <p>&nbsp;</p>
+                    <p><span>Este texto não substitui o publicado no DOU de 26.9.2008</span></p>
+                </div>
+            </section>
         </main>
         <Footer />
         </>
