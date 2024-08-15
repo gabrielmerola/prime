317a5d7b5bf968699937b24ea6552c04ab53ec67
gabrielmerola <gabrielmerola12@gmail.com>
2024-08-15 15:21:28 -0300

    tela de dicas

---
+++ src/components/CardDicas/index.tsx
@@ -0,0 +1,16 @@
+export function CardDicas({ item, index }: any) {
+    return (
+        <div className="p-4" key={index}>
+            <div className="bg-white rounded-xl shadow-md overflow-hidden cursor-pointer duration-300 hover:scale-105">
+                <div className='w-full h-40 overflow-hidden'>
+                    <img className="object-cover" src={item.img} alt={item.title} />
+                </div>
+                <div className="p-4 h-52">
+                    <h3 className="text-lg text-prime-blue duration-500 hover:text-prime-orange font-bold">{item.title}</h3>
+                    <p className="text-[10px] text-prime-blue">{item.description}</p>
+                    <button type="button" className="bg-prime-orange text-white px-2 hover:bg-orange-400">Leia Mais</button>
+                </div>
+            </div>
+        </div>
+    )
+}
--- src/screens/Users/dicas/index.tsx
+++ src/screens/Users/dicas/index.tsx
@@ -1,14 +1,50 @@
 import { Footer } from "../../../components/Footer";
 import { Header } from "../../../components/Header";
 import banner from "../../../assets/banner-dicas.png";
+import { CardDicas } from "../../../components/CardDicas";
 
 export function Dicas() {
+    const dicas = [
+        {
+            'img': 'https://via.placeholder.com/500x500',
+            'title': 'Navegando com Cautela – O cuidado com as Informações que buscamos',
+            'description': 'Administração, Dicas, Ensino Superior, Ensino Técnico, Graduação'
+        },
+        {
+            'img': 'https://via.placeholder.com/500x500',
+            'title': 'Trabalho em Equipe. Questão de Sobrevivência?',
+            'description': 'Administração, Administração de Empresas, Dicas, Gestão em Recursos Humanos, Processos Gerenciais'
+        },
+        {
+            'img': 'https://via.placeholder.com/500x500',
+            'title': 'Cuidados com os Pensamentos Nocivos!!!',
+            'description': 'Dicas'
+        },
+        {
+            'img': 'https://via.placeholder.com/500x500',
+            'title': 'Como obter o bom desempenho Profissional?',
+            'description': 'Dicas'
+        },
+        {
+            'img': 'https://via.placeholder.com/500x500',
+            'title': 'Atendimento Humanizado',
+            'description': 'Dicas'
+        },
+    ]
     return (
         <>
         <Header page="dicas"/>
         <main>
             <section className="w-full h-64 overflow-hidden relative">
                 <img className="w-full h-full object-cover object-center" src={banner} alt="Imagem do banner" />
+            </section>
+
+            <section className="flex justify-center">
+                <div className="w-2/3 my-16 grid grid-cols-4 gap-4">
+                    {dicas.map((item, index) => (
+                        <CardDicas item={item} index={index} />
+                    ))}
+                </div>
             </section>
         </main>
         <Footer />
