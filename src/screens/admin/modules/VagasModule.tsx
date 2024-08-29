481d702fd2f97f7242ece3401350e15db941030a
Gabriel Merola <gabrielmerola12@gmail.com>
2024-08-29 17:03:27 -0300

    feat: Add VagasModule component for job listings
    This commit introduces the VagasModule component, which will be used to display and manage job listings in the admin dashboard. The new file src/screens/admin/modules/VagasModule.tsx has been created to implement this functionality.

---
+++ src/screens/admin/modules/VagasModule.tsx
@@ -0,0 +1,7 @@
+export function VagasModule() {
+    return (
+        <section className="w-5/6 bg-[rgba(255,255,255,0.5)] rounded-[48px] p-8">
+            <h1 className="font-bold text-3xl mb-8 uppercase">Vagas</h1>
+        </section>
+    )
+}
