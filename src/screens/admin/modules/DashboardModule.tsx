220703fed0e53f82f237b9965b1f13d63ce75093
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-05 14:24:47 -0300

    ajustes dashboard

--- src/screens/admin/modules/DashboardModule.tsx
+++ src/screens/admin/modules/DashboardModule.tsx
@@ -7,11 +7,11 @@ export function DashboardModule() {
                 <i className="fa-solid fa-bell text-4xl text-prime-orange"></i>
                 <i className="fa-regular fa-user text-4xl text-prime-orange bg-prime-blue px-3 py-2 rounded-full"></i>
             </nav>
-            <div className="flex items-center gap-4 mb-8">
+            <div className="flex items-center gap-4">
                 <i className="fa-solid fa-house text-4xl text-prime-orange"></i>
                 <h1 className="uppercase text-4xl text-prime-blue">Dashboard</h1>
             </div>
-            <div className="grid grid-cols-2 gap-12 p-8 h-full mt-16">
+            <div className="grid grid-cols-2 gap-12 p-8 h-full">
                 <CardDashboard title="Estagiários ativos" description="Quantidade de estagiários ativos no mês" value="50" />
                 <CardDashboard title="Média de bolsa-auxílio" description="Valor médio geral da bolsa-auxílio" value="R$850,00" />
                 <CardDashboard title="Vagas de estágio abertas" description="Cadastrar mais vagas" value="15" />
