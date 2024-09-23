8f71f626a2e4ceb56df7c57f035ac3d24efe7524
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-23 11:03:25 -0300

    adiciona rota de cadastro

--- src/routes/RegisterRoutes.tsx
+++ src/routes/RegisterRoutes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
 import { Login } from '../screens/Register/Login';
+import { Cadastro } from '../screens/Register/Cadastro';
 
 export function RegisterRoutes() {
     return (
         <Routes>
             <Route path='/login' element={<Login />} />
+            <Route path='/cadastro' element={<Cadastro />} />
         </Routes>
     )
 }
