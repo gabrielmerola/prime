e6b256adc14dc61c007cbb98bdd2497019b51bbc
gabrielmerola <gabrielmerola12@gmail.com>
2024-09-24 11:46:02 -0300

    adicionando as imagens na tela de cadastro

--- src/screens/Register/Cadastro/index.tsx
+++ src/screens/Register/Cadastro/index.tsx
@@ -1,7 +1,19 @@
+import './style.css'
+
 export function Cadastro() {
     return (
         <main className="w-full h-screen flex items-center justify-center">
-            <h1>opa</h1>
+            <section id="backgroundAzul" className="h-screen w-1/2 flex items-center justify-center text-white">
+                <img src="" alt="" />
+                <div className='uppercase font-bold text-center text-3xl'>
+                    <h1>Faça o cadastro</h1>
+                    <h1>da sua empresa</h1>
+                    <h1>de estudante</h1>
+                </div>
+            </section>
+            <section className="h-screen w-1/2">
+
+            </section>
         </main>
     )
 }
