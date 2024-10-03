47e0e8c035c2d2c6a28a01240765667d2eb8b06a
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-03 15:28:32 -0300

    testando o setValue

--- src/screens/Register/modules/CadastroEmpresa.tsx
+++ src/screens/Register/modules/CadastroEmpresa.tsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import imagemLaranja from '../../../assets/background/Illustration-orange.png'
-// import { InputMask } from 'primereact/inputmask';
+import { InputMask } from 'primereact/inputmask';
 
 export function CadastroEmpresa({ cargo } : { cargo : string }) {
     const [parte, setParte] = useState(1);
-    // const [value, setValue] = useState('');
-    // setValue('')
-    setParte(1)
+    const [value, setValue] = useState('');
     return (
         <main className="w-full h-screen flex items-center justify-center">
             <section id="backgroundAzul" className={`${cargo == "empresa" ? "flex" : "hidden"} h-screen w-1/2 flex-col gap-4 items-center justify-center text-white`}>
@@ -33,6 +31,132 @@ export function CadastroEmpresa({ cargo } : { cargo : string }) {
                         <span className='font-bold'>3</span>
                     </div>
                 </div>
+                {parte == 1 ?
+                    <div className='w-1/2'>
+                        <form className='mb-4 flex flex-col gap-8' action="">
+                            <div className='flex flex-col'>
+                                <label htmlFor="">CNPJ:</label>
+                                <InputMask className='border-b-2 border-gray-300 p-2 pb-0' value={value} onChange={() => {}} mask="99.999.999/9999-99" />
+                            </div>
+                            <div className='flex flex-col'>
+                                <label htmlFor="">Nome Fantasia:</label>
+                                <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                            </div>
+                            <div className='flex flex-col'>
+                                <label htmlFor="">Razão Social:</label>
+                                <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                            </div>
+                            <div className='flex flex-col'>
+                                <label htmlFor="">Número de Funcionários:</label>
+                                <input className='border-b-2 border-gray-300 p-2 pb-0' type="number" />
+                            </div>
+                        </form>
+                        <div className='flex justify-center'>
+                            <button className='bg-gradient-to-b to-70% from-black to-prime-blue text-white px-8 py-4 rounded-full font-bold' onClick={()=>setParte(2)}>próximo</button>
+                        </div>
+                    </div>
+                    : parte == 2 ?
+                    <div className='w-3/4'>
+                        <form className='mb-4 flex flex-col gap-8' action="">
+                            {/* 1 parte */}
+                            <div className='flex gap-8'>
+                                <div className='w-full flex flex-col'>
+                                    <label htmlFor="">Email da empresa:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="email" />
+                                </div>
+                                <div className='w-full flex flex-col'>
+                                    <label htmlFor="">Telefone:</label>
+                                    <InputMask className='border-b-2 border-gray-300 p-2 pb-0' value={value} onChange={(e) => setValue(e.target.value!)} mask="(99) 99999-9999" />
+                                </div>
+                            </div>
+                            {/* 2 parte */}
+                            <div className='flex gap-8'>
+                                <div className='w-1/4 flex flex-col'>
+                                    <label htmlFor="">CEP:</label>
+                                    <InputMask className='border-b-2 border-gray-300 p-2 pb-0' value={value} onChange={() => {}} mask="99999-999" />
+                                </div>
+                                <div className='w-3/4 flex flex-col'>
+                                    <label htmlFor="">Endereço:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                                </div>
+                            </div>
+                            {/* 3 parte */}
+                            <div className='flex gap-8'>
+                                <div className='w-1/5 flex flex-col'>
+                                    <label htmlFor="">Número:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="number" />
+                                </div>
+                                <div className='w-3/5 flex flex-col'>
+                                    <label htmlFor="">Complemento:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                                </div>
+                                <div className='w-1/5 flex flex-col'>
+                                    <label htmlFor="">Bairro:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                                </div>
+                            </div>
+                            {/* 4 parte */}
+                            <div className='flex gap-8'>
+                                <div className='w-full flex flex-col'>
+                                    <label htmlFor="">Cidade:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                                </div>
+                                <div className='w-full flex flex-col'>
+                                    <label htmlFor="">Estado:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                                </div>
+                            </div>
+                        </form>
+                        <div className='flex items-center justify-center gap-2'>
+                            <button className='bg-white border-2 border-prime-blue px-8 py-4 rounded-full font-bold' onClick={()=>setParte(1)}>voltar</button>
+                            <button className='bg-gradient-to-b to-70% from-black to-prime-blue text-white px-8 py-4 rounded-full font-bold' onClick={()=>setParte(3)}>próximo</button>
+                        </div>
+                    </div>
+                    :
+                    <div className='w-3/4'>
+                        <p className='text-center text-prime-blue mb-4'>Precisamos cadastrar um representante para essa empresa. Por favor, informe os seguintes dados:</p>
+                        <form className='mb-4 flex flex-col gap-8' action="">
+                            <div className='w-full flex flex-col'>
+                                <label htmlFor="">Nome civil:</label>
+                                <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                            </div>
+                            {/* 1 parte */}
+                            <div className='flex items-end gap-8'>
+                                <div className='w-1/4 flex flex-col'>
+                                    <label htmlFor="">Data de Nascimento:</label>
+                                    <InputMask className='border-b-2 border-gray-300 p-2 pb-0' value={value} onChange={() => {}} mask="99/99/9999" />
+                                </div>
+                                <div className='w-2/4 flex flex-col'>
+                                    <label htmlFor="">CPF:</label>
+                                    <InputMask className='border-b-2 border-gray-300 p-2 pb-0' value={value} onChange={() => {}} mask="999.999.999-99" />
+                                </div>
+                                <div className='w-1/4 flex flex-col'>
+                                    <label htmlFor="">Código do Conselho:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="text" />
+                                </div>
+                            </div>
+                            <div className='w-full flex flex-col'>
+                                <label htmlFor="">Email de Acesso:</label>
+                                <input className='border-b-2 border-gray-300 p-2 pb-0' type="email" />
+                            </div>
+                            {/* 2 parte */}
+                            <div className='flex gap-8'>
+                                <div className='w-1/2 flex flex-col'>
+                                    <label htmlFor="">Senha de Acesso:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="password" />
+                                </div>
+                                <div className='w-1/2 flex flex-col'>
+                                    <label htmlFor="">Confirmação de senha:</label>
+                                    <input className='border-b-2 border-gray-300 p-2 pb-0' type="password" />
+                                </div>
+                            </div>
+                        </form>
+                        <div className='flex items-center justify-center gap-2'>
+                            <button className='bg-white border-2 border-prime-blue px-8 py-4 rounded-full font-bold' onClick={()=>setParte(2)}>voltar</button>
+                            <button className='bg-gradient-to-b to-70% from-black to-prime-blue text-white px-8 py-4 rounded-full font-bold' onClick={()=>{}}>finalizar</button>
+                        </div>
+                    </div>
+                }
             </section>
         </main>
     )
