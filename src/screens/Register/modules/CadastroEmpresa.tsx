3c7c7eb87f7fbcf4058b35e18a9ee0fe83a08598
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-03 15:26:18 -0300

    teste removendo partes

--- src/screens/Register/modules/CadastroEmpresa.tsx
+++ src/screens/Register/modules/CadastroEmpresa.tsx
@@ -1,9 +1,9 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import imagemLaranja from '../../../assets/background/Illustration-orange.png'
 // import { InputMask } from 'primereact/inputmask';
 
 export function CadastroEmpresa({ cargo } : { cargo : string }) {
-    // const [parte, setParte] = useState(1);
+    const [parte, setParte] = useState(1);
     // const [value, setValue] = useState('');
     // setValue('')
     return (
@@ -16,7 +16,23 @@ export function CadastroEmpresa({ cargo } : { cargo : string }) {
                 </div>
             </section>
 
-            
+            <section className={`${cargo == 'empresa' ? "flex" : "hidden"} h-screen w-1/2 flex-col gap-4 items-center justify-center p-16`}>
+                <h1 className='text-3xl text-prime-blue font-bold'>cadastro de Empresa</h1>
+                <div className='flex items-center justify-between w-full relative'>
+                    <div className='absolute top-0 w-full h-full flex items-center justify-center'>
+                        <div className='w-full h-[2px] bg-black' />
+                    </div>
+                    <div className={`${parte >= 1 ? 'bg-gradient-to-b to-70% from-black to-prime-blue text-white' : 'bg-gray-300'} border-2 border-prime-blue z-10 h-12 w-12 rounded-full flex items-center justify-center`}>
+                        <span className='font-bold'>1</span>
+                    </div>
+                    <div className={`${parte >= 2 ? 'bg-gradient-to-b to-70% from-black to-prime-blue text-white' : 'bg-gray-300'} border-2 border-prime-blue z-10 h-12 w-12 rounded-full flex items-center justify-center`}>
+                        <span className='font-bold'>2</span>
+                    </div>
+                    <div className={`${parte >= 3 ? 'bg-gradient-to-b to-70% from-black to-prime-blue text-white' : 'bg-gray-300'} border-2 border-prime-blue z-10 h-12 w-12 rounded-full flex items-center justify-center`}>
+                        <span className='font-bold'>3</span>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
