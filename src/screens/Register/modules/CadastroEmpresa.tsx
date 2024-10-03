2bd34e52cfa8c81289de7b9b1de6e8c5258ed362
gabrielmerola <gabrielmerola12@gmail.com>
2024-10-03 15:24:52 -0300

    removendo imports e useStates

--- src/screens/Register/modules/CadastroEmpresa.tsx
+++ src/screens/Register/modules/CadastroEmpresa.tsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import imagemLaranja from '../../../assets/background/Illustration-orange.png'
-import { InputMask } from 'primereact/inputmask';
+// import { InputMask } from 'primereact/inputmask';
 
 export function CadastroEmpresa({ cargo } : { cargo : string }) {
-    const [parte, setParte] = useState(1);
-    const [value, setValue] = useState('');
-    setValue('')
+    // const [parte, setParte] = useState(1);
+    // const [value, setValue] = useState('');
+    // setValue('')
     return (
         <main className="w-full h-screen flex items-center justify-center">
             <section id="backgroundAzul" className={`${cargo == "empresa" ? "flex" : "hidden"} h-screen w-1/2 flex-col gap-4 items-center justify-center text-white`}>
